5aebeaa1675e5c07511ab6e0a64087d4a9f831c7
russian_proger <sagitov.2806@mail.ru>
2025-02-03 21:23:52 +0300

    feat: add base layout

--- index/views.py
+++ index/views.py
@@ -2,6 +2,7 @@ from django.template import loader
 from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
-def index(request: HttpRequest):
+def index(_request: HttpRequest):
+    """ Main page """
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
