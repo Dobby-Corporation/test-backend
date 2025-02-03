44e04bbe72b16fac652d6516e6447ed230e9159e
russian_proger <sagitov.2806@mail.ru>
2025-02-03 22:03:20 +0300

    feat(tests): add models, index view

--- test_booster/urls.py
+++ test_booster/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('index.urls')),
+    path('', include('index.urls'), name='index'),
+    path('tests/', include('testings.urls'), name='tests'),
     path('admin/', admin.site.urls),
 ]
--- testings/models.py
+++ testings/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    """ Test model """
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=60)
+
+class TestVersion(models.Model):
+    """ Test version model """
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+class Task(models.Model):
+    """ Task model """
+    test_version = models.ForeignKey(TestVersion, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+
+class QuizTask(models.Model):
+    """ Quiz task model """
+    description = models.CharField(max_length=500)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+class ProgramTask(models.Model):
+    """ Program task model """
+    description = models.CharField(max_length=1000)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+class ProgramTaskTestCase(models.Model):
+    """ Program task test case model """
+    program_task = models.ForeignKey(ProgramTask, on_delete=models.CASCADE)
+    input = models.CharField(max_length=1000)
+    output = models.CharField(max_length=1000)
